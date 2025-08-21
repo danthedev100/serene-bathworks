cb8e70fd10c2c1c5a84f5892b54fa67154c52b83
danthedev100 <danthedev100@gmail.com>
2025-08-21 20:02:32 +0200

    changes to way it displays made

---
+++ lib/format.ts
@@ -0,0 +1,8 @@
+// lib/format.ts
+export function formatZAR(value: number) {
+  return new Intl.NumberFormat('en-ZA', {
+    style: 'currency',
+    currency: 'ZAR',
+    maximumFractionDigits: 0
+  }).format(value);
+}
